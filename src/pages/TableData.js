5a030c8cd509f8232d17e9de46ed399675dadd0c
agusandriputra <andriputra.job@gmail.com>
2021-05-01 06:33:07 +0700

    modify style

--- src/pages/TableData.js
+++ src/pages/TableData.js
@@ -4,6 +4,8 @@ import { AddNewData } from '../component/modal/AddNewData'
 import { EditData } from '../component/modal/EditData'
 import { Context } from '../config/context'
 import { Search } from './Search'
+import logo from '../assets/img/logo.png'
+import { FaTrashAlt, FaEdit } from 'react-icons/fa'
 
 export const TableData = () => {
   const [data, setData] = useState([])
@@ -19,8 +21,6 @@ export const TableData = () => {
 
 
   const getData = async () => {
-
-
     await fetch(
       `https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/list`,
       {
@@ -34,7 +34,6 @@ export const TableData = () => {
       .then(res => {
 
         setData(res)
-        // console.log(res)
         setDataTemp(res)
       })
       .catch(e => {
@@ -78,9 +77,8 @@ export const TableData = () => {
   }, [props])
 
   console.log(data[index])
-
-
-  console.log('iini data', dataTemp)
+  console.log('data test', dataTemp)
+  
   const onChangeValue = (e) => {
     setSearch(e.target.value)
   }
@@ -98,12 +96,7 @@ export const TableData = () => {
     let reg = new RegExp(pattern, "g")
 
     data2 = data.filter(item => item.area_kota !== null ? item.area_kota.toLowerCase().includes(`${search}`) : item.area_kota)
-
-
-
-
     setDataTemp(data2)
-
     console.log(data2)
   }
 
@@ -163,10 +156,13 @@ export const TableData = () => {
 
 
   return (
-    <div id="tabledata" className="container">
-      {/* {JSON.stringify(data.filter(price => price.price !== null && price.size !== null && price.area_kota !== null && price.area_provinsi !== null)[index])} */}
+    <div id="Tabledata" className="container">
+      <div className="card">
       <div className="row">
         <div className="col-md-3 col-sm-12">
+          <img src={logo} className="main-logo img-fluid" alt="efishery logo"/>
+        </div>
+        <div className="col-md-7 col-sm-12">
           <Search
             search={search}
             onChangeValue={onChangeValue}
@@ -174,63 +170,61 @@ export const TableData = () => {
             btnSearch={btnSearch}
           />
         </div>
-        <div className="col-md-9 col-sm-12">
-          <span className="btn btn-primary" onClick={() => setisopenadd(!isopenadd)}>
+        <div className="col-md-2 col-sm-12">
+          <button type="button" className="btn btn-success btn-block" onClick={() => setisopenadd(!isopenadd)}>
             Add Data
-          </span>
+          </button>
         </div>
       </div>
       <div className="row mt-5">
         <div className="col-md-12 col-sm-12" >
-          <table className="table table-bordered">
-            <thead className="header">
-              <tr>
-                <th>No</th>
-                <th>Provinsi</th>
-                <th>
-                  City
-                </th>
-                <th
-                  style={{ cursor: 'pointer', alignItems: 'center', justifyContent: 'center' }}
-                  onClick={btnSortSize}>
-                  Size
-                  {
-                    sortsize === 'asc' ? <FaCaretUp /> : <FaCaretDown />
-                  }
-                </th>
-                <th
-                  style={{ cursor: 'pointer', alignItems: 'center', justifyContent: 'center' }}
-                  onClick={btnSortPrice}
-                >
-                  Price
-                  {
-                    sortprice === 'asc' ? <FaCaretUp /> : <FaCaretDown />
-                  }
-                </th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody className="body-table">
-              {
-                dataTemp.filter(price => price.price !== null && price.size !== null && price.area_kota !== null && price.area_provinsi !== null).map((item, index) => (
-                  <tr key={index} className={`${index % 2 ? 'ganjil' : 'genap'}`}>
-                    <th> {index + 1} </th>
-                    <td> {item.area_provinsi} </td>
-                    <td> {item.area_kota} </td>
-                    <td> {item.size} </td>
-                    <td> {item.price} </td>
-                    <td>
-                      <span className="btn btn-primary"
-                        onClick={() => btnEdit(index)}
-                      >Edit</span> &nbsp;&nbsp;
-                      <span className="btn btn-danger" onClick={() => btnDelete(index)}>Delete</span>
-                    </td>
-                  </tr>
-                ))
+          <div className="table-responsive">
+            <table className="table table-striped">
+              <thead className="header">
+                <tr>
+                  <th>No</th>
+                  <th>Provinsi</th>
+                  <th>City</th>
+                  <th
+                    style={{ cursor: 'pointer', alignItems: 'center', justifyContent: 'center' }}
+                    onClick={btnSortSize}>
+                    Size
+                    {
+                      sortsize === 'asc' ? <FaCaretUp /> : <FaCaretDown />
+                    }
+                  </th>
+                  <th
+                    style={{ cursor: 'pointer', alignItems: 'center', justifyContent: 'center' }}
+                    onClick={btnSortPrice}
+                  >
+                    Price
+                    {
+                      sortprice === 'asc' ? <FaCaretUp /> : <FaCaretDown />
+                    }
+                  </th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody className="body-table">
+                {
+                  dataTemp.filter(price => price.price !== null && price.size !== null && price.area_kota !== null && price.area_provinsi !== null).map((item, index) => (
+                    <tr key={index} className={`${index % 2 ? 'odd' : 'even'}`}>
+                      <th> {index + 1} </th>
+                      <td> {item.area_provinsi} </td>
+                      <td> {item.area_kota} </td>
+                      <td> {item.size} </td>
+                      <td> {item.price} </td>
+                      <td className="action-btn">
+                        <button className="btn btn-primary" onClick={() => btnEdit(index)}><FaEdit/></button>
+                        <button className="btn btn-danger" onClick={() => btnDelete(index)}><FaTrashAlt/></button>
+                      </td>
+                    </tr>
+                  ))
 
-              }
-            </tbody>
-          </table>
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
@@ -249,6 +243,7 @@ export const TableData = () => {
             index={index}
           />
         </div>
+      </div>
       </div>
     </div>
   )
